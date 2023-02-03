821f7778dfab163cd6b4c5fd45df02a96bda1ad5
SSbuding <2771576733@qq.com>
2023-02-03 22:54:12 +0800

    Route authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-
-// 导入路由
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-// 导入页面组件
-import Login from '@/pages/Login'
+import { AuthRoute } from '@/components/AuthRoute'
 import Layout from '@/pages/Layout'
+import Login from '@/pages/Login'
 
-// 配置路由规则
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* 需要鉴权的路由 */}
+        <Route path="/*" element={
+          <AuthRoute>
+            <Layout />
+          </AuthRoute>
+        } />
+        {/* 不需要鉴权的路由 */}
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </BrowserRouter>
   )
 }
-
 export default App
